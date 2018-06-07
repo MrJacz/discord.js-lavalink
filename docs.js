a5dea17f67d1b99a66920b10c37243623fa7a937
Jacz <jacz@jaczaus.me>
2018-06-07 22:42:46 +1000

    maybe?

--- docs.js
+++ docs.js
@@ -1,81 +1,80 @@
 const Docma = require("docma");
 
-Docma.create()
-    .build({
-        app: {
-            title: "Discord.js LavaLink",
-            base: "/discord.js-lavalink",
-            entrance: "content:readme",
-            routing: "query",
-            server: Docma.ServerType.GITHUB
-        },
-        template: {
-            path: "zebra",
-            options: {
-                title: {
-                    label: "Discord.js LavaLink",
-                    href: "/discord.js-lavalink?"
-                },
-                sidebar: {
-                    enabled: true,
-                    outline: "tree",
-                    collapsed: false,
-                    toolbar: true,
-                    itemsFolded: false,
-                    itemsOverflow: "crop",
-                    badges: true,
-                    search: true,
-                    animations: true
-                },
-                symbols: {
-                    autoLink: true,
-                    params: "list",
-                    enums: "list",
-                    props: "list",
-                    meta: false
-                },
-                navbar: {
-                    enabled: true,
-                    fixed: true,
-                    dark: false,
-                    animations: true,
-                    menu: [
-                        {
-                            label: "Readme",
-                            href: "?content=readme"
-                        },
-                        {
-                            label: "Documentation",
-                            href: "?api=lavalink",
-                            iconClass: "fas fa-book"
-                        },
-                        {
-                            iconClass: "fab fa-lg fa-github",
-                            label: "",
-                            href: "https://github.com/MrJacz/discord.js-lavalink",
-                            target: "_blank"
-                        }
-                    ]
-                }
+new Docma().build({
+    app: {
+        title: "Discord.js LavaLink",
+        base: "/discord.js-lavalink",
+        entrance: "content:readme",
+        routing: "query",
+        server: Docma.ServerType.GITHUB
+    },
+    template: {
+        path: "zebra",
+        options: {
+            title: {
+                label: "Discord.js LavaLink",
+                href: "/discord.js-lavalink?"
+            },
+            sidebar: {
+                enabled: true,
+                outline: "tree",
+                collapsed: false,
+                toolbar: true,
+                itemsFolded: false,
+                itemsOverflow: "crop",
+                badges: true,
+                search: true,
+                animations: true
+            },
+            symbols: {
+                autoLink: true,
+                params: "list",
+                enums: "list",
+                props: "list",
+                meta: false
+            },
+            navbar: {
+                enabled: true,
+                fixed: true,
+                dark: false,
+                animations: true,
+                menu: [
+                    {
+                        label: "Readme",
+                        href: "?content=readme"
+                    },
+                    {
+                        label: "Documentation",
+                        href: "?api=lavalink",
+                        iconClass: "fas fa-book"
+                    },
+                    {
+                        iconClass: "fab fa-lg fa-github",
+                        label: "",
+                        href: "https://github.com/MrJacz/discord.js-lavalink",
+                        target: "_blank"
+                    }
+                ]
             }
-        },
-        markdown: {
-            gfm: true,
-            tables: true,
-            breaks: true,
-            pedantic: true,
-            sanitize: true,
-            smartLists: true,
-            smartypants: true,
-            tasks: true,
-            emoji: true
-        },
-        src: [
-            { readme: "./README.md" },
-            { lavalink: "./src/**/*.js" }
-        ],
-        dest: "./docs",
-        debug: true,
-        jsdoc: { package: "./package.json" }
-    })
+        }
+    },
+    markdown: {
+        gfm: true,
+        tables: true,
+        breaks: true,
+        pedantic: true,
+        sanitize: true,
+        smartLists: true,
+        smartypants: true,
+        tasks: true,
+        emoji: true
+    },
+    src: [
+        { readme: "./README.md" },
+        { lavalink: "./src/*/**/**/*.js" }
+    ],
+    dest: "./docs",
+    debug: 31,
+    jsdoc: { package: "./package.json" }
+})
     .catch(console.error);
