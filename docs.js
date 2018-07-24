2172099440b0714cb7b3e0df110e29cfecb23988
Jacz <jacz@jaczaus.me>
2018-07-25 08:23:03 +1000

    Update docs.js

--- docs.js
+++ docs.js
@@ -1,41 +1,37 @@
 const Docma = require("docma");
+const Package = require("./package");
 
-new Docma().build({
-    app: {
-        title: "Discord.js LavaLink",
-        base: "/discord.js-lavalink",
-        entrance: "content:readme",
-        routing: "query",
-        server: "github"
-    },
-    template: {
-        path: "zebra",
-        options: {
-            title: "Discord.js LavaLink",
-            sidebar: {
-                enabled: true,
-                outline: "tree",
-                collapsed: false,
-                toolbar: true,
-                itemsFolded: false,
-                itemsOverflow: "crop",
-                badges: true,
-                search: true,
-                animations: true
-            },
-            symbols: {
-                autoLink: true,
-                params: "list",
-                enums: "list",
-                props: "list",
-                meta: false
-            },
-            navbar: {
-                enabled: true,
-                fixed: true,
-                dark: false,
-                animations: true,
-                menu: [
+Docma.create()
+    .build({
+        app: {
+            title: Package.name,
+            base: "/discord.js-lavalink",
+            entrance: "content:readme",
+            routing: "query",
+            server: Docma.ServerType.GITHUB
+        },
+        markdown: {
+            gfm: true,
+            tables: true,
+            breaks: true,
+            pedantic: true,
+            sanitize: true,
+            smartLists: true,
+            smartypants: true,
+            tasks: true,
+            emoji: true
+        },
+        src: [
+            { readme: "./README.md" },
+            { lavalink: "./src/*/**/*.js" }
+        ],
+        dest: "./docs",
+        debug: true,
+        jsdoc: { package: "./package.json" },
+        template: {
+            options: {
+                title: Package.name,
+                navItems: [
                     {
                         label: "Readme",
                         href: "?content=readme"
@@ -43,52 +39,16 @@ new Docma().build({
                     {
                         label: "Documentation",
                         href: "?api=lavalink",
-                        iconClass: "fas fa-book"
+                        iconClass: "ico-book"
                     },
                     {
-                        iconClass: "fab fa-lg fa-github",
-                        label: "",
-                        href: "https://github.com/MrJacz/discord.js-lavalink",
-                        target: "_blank"
+                        label: "GitHub",
+                        href: Package.homepage,
+                        target: "_blank",
+                        iconClass: "ico-md ico-github"
                     }
                 ]
             }
         }
-    },
-    markdown: {
-        gfm: true,
-        tables: true,
-        breaks: true,
-        pedantic: true,
-        sanitize: true,
-        smartLists: true,
-        smartypants: true,
-        tasks: true,
-        emoji: true
-    },
-    src: [
-        { readme: "./README.md" },
-        { lavalink: "./src/*/**/**/*.js" }
-    ],
-    dest: "./docs",
-    debug: 5,
-    jsdoc: {
-        encoding: "utf8",
-        recurse: false,
-        pedantic: false,
-        access: null,
-        package: "./package.json",
-        module: true,
-        undocumented: false,
-        undescribed: false,
-        ignored: false,
-        hierarchy: true,
-        sort: "alphabetic",
-        relativePath: null,
-        filter: null,
-        allowUnknownTags: true,
-        plugins: []
-    }
-})
-    .then(() => console.log("Documentation is built successfully."))
+    })
     .catch(console.error);
